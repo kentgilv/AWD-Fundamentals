44f2ce7f3b73657360d5d8d73f6c5a7f949efdd7
kentgilv <kent.v.valencia@gmail.com>
2025-01-25 23:07:38 +0800

    11:07 pm

--- pages/page1/assets/js/scripts.js
+++ pages/page1/assets/js/scripts.js
@@ -1,7 +1,42 @@
-const your_name =  document
+function showInput()
+{
+    const userInput = document.getElementById("userInput").value;
+    const output = document.getElementById("output");
 
-document.getElementById("name").innerHTML = your_name;
+    if (userInput) {
+        output.textContent = `You entered:  ${userInput}`;
+    }
+    else {
+        output.textContent = "Please enter something.";
+    }
+}
 
-if (new Date().getHours() < 18) {
-    document.getElementById("demo").innerHTML = "Good day!";
+function checkOddEven() {
+    const numberInput = document.getElementById("numberInput").value;
+    const conditionalOutput = document.getElementById("conditionalOutput");
+
+    if (numberInput === "") {
+        conditionalOutput.textContent = "Please enter a number.";
+    }
+    else {
+        const number = parseInt(numberInput, 10);
+        if (number % 2 === 0) {
+            conditionalOutput.textContent = `${number} is even.`;
+        }
+        else {
+            conditionalOutput.textContent = `${number} is odd.`;
+        }
+    }
+}
+
+function displayNumbers() {
+    const loopOutput = document.getElementById("loopOutput");
+
+    loopOutput.innerHTML = "";
+
+    for (let i = 1; i <= 7; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = i;
+        loopOutput.appendChild(listItem);
+    }
 }
